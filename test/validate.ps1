148f872d0feccd2312389a6b3342a8ce82feb784
Bernard <bernard@ducourier.com>
2019-05-13 08:35:11 -0400

    Change validation logic

--- test/validate.ps1
+++ test/validate.ps1
@@ -1,34 +1,18 @@
 Param(
-    [Parameter(Mandatory = $True)][string]$templateLibraryName = "name of template",
-    [Parameter(Mandatory = $True)][string]$templateLibraryVersion = "version of template",
+    [Parameter(Mandatory = $false)][string]$templateLibraryName = "servers",
     [string]$templateName = "azuredeploy.json",
-    [string]$containerName = "library-dev",
-    [string]$prodContainerName = "library",
-    [string]$storageRG = "PwS2-Infra-Storage-RG",
-    [string]$storageAccountName = "azpwsdeploytpnjitlh3orvq",
     [string]$Location = "canadacentral"
 )
 
-function Output-DeploymentName {
-    param( [string]$Name)
-
-    $pattern = '[^a-zA-Z0-9-]'
-
-    # Remove illegal characters from deployment name
-    $Name = $Name -replace $pattern, ''
-
-    # Truncate deplayment name to 64 characters
-    $Name.subString(0, [System.Math]::Min(64, $Name.Length))
-}
-$devBaseTemplateUrl = "https://$storageAccountName.blob.core.windows.net/$containerName/arm"
-$prodBaseTemplateUrl = "https://$storageAccountName.blob.core.windows.net/$prodContainerName/arm"
-$gcLibraryUrl = "https://raw.githubusercontent.com/canada-ca/Templates/arm"
-$serversDevURL = "https://raw.githubusercontent.com/canada-ca-arm/servers/dev/azuredeploy.json"
+$gcLibraryUrl = "https://raw.githubusercontent.com/canada-ca/accelerators_accelerateurs-azure/master/Templates/arm"
+$serversDevURL = "https://raw.githubusercontent.com/canada-ca-arm/dev/servers/azuredeploy.json"
 
 #******************************************************************************
 # Script body
 # Execution begins here
 #******************************************************************************
+
+Select-AzureRmSubscription PwS2-CCC-Validation
 
 # Cleanup old jobs
 Get-Job | Remove-Job
@@ -36,6 +20,16 @@ Get-Job | Remove-Job
 # Start the deployment
 Write-Host "Starting deployment...";
 
+New-AzureRmDeployment -Location $Location -Name "Deploy-Infrastructure-Dependancies" -TemplateUri "https://raw.githubusercontent.com/canada-ca/accelerators_accelerateurs-azure/master/Templates/arm/masterdeploy/20190319.1/masterdeploysub.json" -TemplateParameterFile (Resolve-Path -Path "$PSScriptRoot\masterdeploysub.parameters.json") -Verbose;
+
+$provisionningState = (Get-AzureRmDeployment -Name "Deploy-Infrastructure-Dependancies").ProvisioningState
+
+if ($provisionningState -eq "Failed") {
+    Write-Host "One of the jobs was not successfully created... exiting..."
+    exit
+}
+
+<#
 # Building dependencies needed for the server validation
 New-AzureRmDeployment -Location $Location -Name "dependancy-$templateLibraryName-Build-resourcegroups" -TemplateUri "$gcLibraryUrl/resourcegroups/20190207.2/$templateName" -TemplateParameterFile (Resolve-Path "$PSScriptRoot\dependancy-resourcegroups-canadacentral.parameters.json") -Verbose
 New-AzureRmResourceGroupDeployment -ResourceGroupName PwS2-validate-servers-1-RG -Name "dependancy-$templateLibraryName-Build-keyvaults" -TemplateUri "$gcLibraryUrl/keyvaults/20190302.1/$templateName" -TemplateParameterFile (Resolve-Path "$PSScriptRoot\dependancy-keyvaults.parameters.json") -_debugLevel "requestContent,responseContent" -Verbose -AsJob
@@ -53,6 +47,7 @@ if (Get-Job -State Failed) {
 
 # Cleanup old jobs before running new deployments
 Get-Job | Remove-Job
+#>
 
 # Validating server template
 New-AzureRmResourceGroupDeployment -ResourceGroupName PwS2-validate-servers-1-RG -Name "validate-$templateLibraryName-Build-$templateLibraryName" -TemplateUri $serversDevURL -TemplateParameterFile (Resolve-Path "$PSScriptRoot\validate-servers.parameters.json") -Verbose
@@ -66,6 +61,3 @@ if ($provisionningState -eq "Failed") {
 # Cleanup validation resource content
 Write-Host "Cleanup validation resource content...";
 New-AzureRmResourceGroupDeployment -ResourceGroupName PwS2-validate-servers-1-RG -Mode Complete -TemplateFile (Resolve-Path "$PSScriptRoot\cleanup.json") -Force -Verbose
-
-Get-Job | Wait-Job
-Get-Job | Receive-Job

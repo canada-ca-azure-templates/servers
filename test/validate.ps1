3c0e068c3468344d8ece7edd9962a5a98ff8274e
Bernard <bernard@ducourier.com>
2019-05-13 08:47:50 -0400

    Fix issue with parameter Cleanup validation script

--- test/validate.ps1
+++ test/validate.ps1
@@ -4,8 +4,7 @@ Param(
     [string]$Location = "canadacentral"
 )
 
-$gcLibraryUrl = "https://raw.githubusercontent.com/canada-ca/accelerators_accelerateurs-azure/master/Templates/arm"
-$serversDevURL = "https://raw.githubusercontent.com/canada-ca-arm/dev/servers/azuredeploy.json"
+$serversDevURL = "https://raw.githubusercontent.com/canada-ca-arm/servers/dev/azuredeploy.json"
 
 #******************************************************************************
 # Script body
@@ -13,9 +12,6 @@ $serversDevURL = "https://raw.githubusercontent.com/canada-ca-arm/dev/servers/az
 #******************************************************************************
 
 Select-AzureRmSubscription PwS2-CCC-Validation
-
-# Cleanup old jobs
-Get-Job | Remove-Job
 
 # Start the deployment
 Write-Host "Starting deployment...";
@@ -28,26 +24,6 @@ if ($provisionningState -eq "Failed") {
     Write-Host "One of the jobs was not successfully created... exiting..."
     exit
 }
-
-<#
-# Building dependencies needed for the server validation
-New-AzureRmDeployment -Location $Location -Name "dependancy-$templateLibraryName-Build-resourcegroups" -TemplateUri "$gcLibraryUrl/resourcegroups/20190207.2/$templateName" -TemplateParameterFile (Resolve-Path "$PSScriptRoot\dependancy-resourcegroups-canadacentral.parameters.json") -Verbose
-New-AzureRmResourceGroupDeployment -ResourceGroupName PwS2-validate-servers-1-RG -Name "dependancy-$templateLibraryName-Build-keyvaults" -TemplateUri "$gcLibraryUrl/keyvaults/20190302.1/$templateName" -TemplateParameterFile (Resolve-Path "$PSScriptRoot\dependancy-keyvaults.parameters.json") -_debugLevel "requestContent,responseContent" -Verbose -AsJob
-New-AzureRmResourceGroupDeployment -ResourceGroupName PwS2-validate-servers-1-RG -Name "dependancy-$templateLibraryName-Build-vnet-subnet-1" -TemplateUri "$gcLibraryUrl/vnet-subnet/20190302.2/$templateName" -TemplateParameterFile (Resolve-Path "$PSScriptRoot\dependancy-vnet-subnet.parameters.json") -_debugLevel "requestContent,responseContent" -Verbose -AsJob
-New-AzureRmResourceGroupDeployment -ResourceGroupName PwS2-validate-servers-1-RG -Name "dependancy-$templateLibraryName-Build-storage-1" -TemplateUri "$gcLibraryUrl/storage/20190302.2/$templateName" -TemplateParameterFile (Resolve-Path "$PSScriptRoot\dependancy-storage.parameters.json") -_debugLevel "requestContent,responseContent" -Verbose -AsJob
-New-AzureRmResourceGroupDeployment -ResourceGroupName PwS2-validate-servers-1-RG -Name "dependancy-$templateLibraryName-Build-availabilityset-1" -TemplateUri "$gcLibraryUrl/availabilityset/20190302.2/$templateName" -TemplateParameterFile (Resolve-Path "$PSScriptRoot\dependancy-availabilityset.parameters.json") -_debugLevel "requestContent,responseContent" -Verbose -AsJob
-
-Get-Job | Wait-Job
-Get-Job | Receive-Job
-
-if (Get-Job -State Failed) {
-    Write-Host "One of the jobs was not successfully created... exiting..."
-    exit
-}
-
-# Cleanup old jobs before running new deployments
-Get-Job | Remove-Job
-#>
 
 # Validating server template
 New-AzureRmResourceGroupDeployment -ResourceGroupName PwS2-validate-servers-1-RG -Name "validate-$templateLibraryName-Build-$templateLibraryName" -TemplateUri $serversDevURL -TemplateParameterFile (Resolve-Path "$PSScriptRoot\validate-servers.parameters.json") -Verbose
